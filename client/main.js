d73793d4008977f5ff06313016293c75f340eab0
Sandor <bakos.webd@gmail.com>
2018-10-06 00:55:32 +0200

    mb up and running need refactor

--- client/main.js
+++ client/main.js
@@ -2,13 +2,46 @@ $("document").ready(() => {
     const socket = io();
     var username = undefined;
 
-    socket.on("connect", function () {
+    //dealing with the username
+    const emittingName = () => {
+        socket.emit("receive_username", $("#user-name").val(), (data) => {
+            if (data) {
+                username = $("#user-name").val()
+                $(".user-modal").modal("hide")
+                $(".hello-user").text(`Hello ${username}`)
+            } else {
+                if (username !== false) {
+                    $(".model-to-append").append("<p style='color: red'>The username is used</p>")
+                }
+                username = false
+            }
+        })
+    }
+    $("#button-username").click(emittingName)
+    $("#user-name").keypress((event) => {
+        if (event.which === 13) {
+            emittingName()
+        }
+    })
+
+    socket.on("server_mb_msg", (arr) => {
+        console.log(arr)
+        arr.forEach(element => {
+            $(".mb-content").prepend(
+                `<p><span class="bg-info text-light">${element.username}</span> | <span class="bg-light">${element.date}</span> | <span class="text-secondary">${element.message}</span></p>`
+            )
+        });
+    })
+
+    socket.on("connect", () => {
+        //listening for all meessages come from other users
         socket.on("server_msg", (msg) => {
             console.log("msg form server :", msg)
             appendMessage(msg[0], msg[1])
         })
     })
     
+    //adding the styled chat elements to the chat
     const appendMessage = (un, msg) => {
         if (un !== username) {
             $(".message-s").prepend(
@@ -23,31 +56,39 @@ $("document").ready(() => {
                 </div>`
             )
         }
-        
     }
 
-    $("#button-username").click(() => {
-        socket.emit("receive_username", $("#user-name").val(), (data) =>{
-            if (data) {
-                username = $("#user-name").val()
-                $(".user-modal").modal("hide")
-            } else {
-                if (username !== false) {
-                    $(".model-to-append").append("<p style='color: red'>The username is used</p>")
-                }
-                username = false
-            }
-        })
-    })
-
-    $("#button-addon").click(() => {
+    //emitting chat messages
+    const emittingChat = () => {
         if ( $("#chat-input").val().length >= 1 ) {
             socket.emit("user_msg", [username, $("#chat-input").val()])
+            $("#chat-input").val("")
         } else {
             // some buzzing animation
         }
+    }
+    $("#button-addon").click(emittingChat)
+    $("#chat-input").keypress((event) => {
+        if (event.which === 13) {
+            emittingChat()
+        }
     })
-    
+
+    //emitting to the msg board
+    const emittingToMbrd = () => {
+        if ( $("#mb-input").val().length >= 1 ) {
+            socket.emit("mb_msg", [username, $("#mb-input").val()])
+            $("#mb-input").val("")
+        } else {
+            // some buzzing animation
+        }
+    }
+    $("#mb-button").click(emittingToMbrd)
+    $("#mb-input").keypress((event) => {
+        if (event.which === 13) {
+            emittingToMbrd()
+        }
+    })
 
     //modal instant show as page loads
     $(".user-modal").modal("show")
--- server/index.js
+++ server/index.js
@@ -3,36 +3,66 @@ const express = require("express")
 const socketIO = require("socket.io")
 const serveStatic = require("serve-static")
 const path = require("path")
+const MongoClient = require("mongodb").MongoClient
 
 const app = express()
 const server = http.Server(app)
 const io = socketIO(server)
+
+const mongodbUrl = "mongodb://localhost:27017/"
+var database = undefined
+var dbo = undefined
+MongoClient.connect(mongodbUrl, (err, db) => {
+    if (err) throw err
+    database = db
+    dbo = database.db("chat_app_mb")
+})
 
 const users = []
 
 app.use(serveStatic(path.join(__dirname, "../client")))
 
 io.on('connection', (socket) => {
+    const findThem = () => {
+        dbo.collection("messages").find({}).toArray((error, result) => {
+            if (error) throw error
+            socket.emit("server_mb_msg", result)
+            socket.broadcast.emit("server_mb_msg", result)
+        })
+    }
     console.log('an user connected')
-    socket.on("user_msg", (msg) => {
-        console.log("message from the user: ", msg)
-        socket.emit("server_msg", msg)
-        socket.broadcast.emit("server_msg", msg)
-    })
-    socket.on("disconnect", () => {
-        console.log("user disconnected")
-    })
     socket.on("receive_username", (username, cbFn) => {
         if (users.indexOf(username) === -1) {
             users.push(username)
             console.log("connected usernames: ", users)
+            findThem()
             cbFn(true)
         } else {
             cbFn(false)
         }
     })
+    socket.on("user_msg", (msg) => {
+        console.log("message from the user: ", msg)
+        socket.emit("server_msg", msg)
+        socket.broadcast.emit("server_msg", msg)
+    })
+    socket.on("mb_msg", (msg) => {
+        const objToPass = {
+            username: msg[0],
+            message: msg[1],
+            date: new Date()
+        }
+
+        dbo.collection("messages").insertOne(objToPass, (err, res) => {
+            if (err) throw err
+            findThem()
+        })
+    })
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+    })
 });
 
 server.listen(3000, () => {
-    console.log("serve is listneing on port 3000")
+    console.log("server is listneing on port 3000")
 })
