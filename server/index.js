3a64803e9ab65c33c89177f78b0807f4f823cf85
Sandor <bakos.webd@gmail.com>
2018-10-05 11:47:35 +0200

    sockets are working and listening

--- server/index.js
+++ server/index.js
@@ -12,8 +12,11 @@ app.use(serveStatic(path.join(__dirname, "../client")))
 
 io.on('connection', (socket) => {
     console.log('a user connected')
-    socket.on('disconnect', function(){
-        console.log('user disconnected')
+    socket.on("chat message", (msg) => {
+        console.log("message from the user: ", msg)
+    })
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
     })
 });
 
