220d6006908f387ee62f0f23a61d2801404ba766
Sandor <bakos.webd@gmail.com>
2018-10-05 11:48:30 +0200

    sockets are working and listening 2

--- client/main.js
+++ client/main.js
@@ -1,4 +1,11 @@
 $("document").ready(() => {
     console.log("document ready")
     const socket = io();
+    $("#button-addon").click(() => {
+        if ( $("#chat-input").val().length >= 1 ) {
+            socket.emit("chat message", $("#chat-input").val())
+        } else {
+            // some buzzing animation
+        }
+    })
 })
